f6b6d3cc292082044aa68e2de6ba508d7e4016b3
yunzhongmengji <1806789313@qq.com>
2022-05-02 15:20:11 +0800

    flask login

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask  # 从flask包中导入Flask类
-from config import Config
+from config import Config  # 从config模块导入Config类
+
+from flask_sqlalchemy import SQLAlchemy  # 丛包中导入类
+from flask_migrate import Migrate
 
 app = Flask(__name__)  # 将Flask类的实例 赋值给名为 app 的变量。这个实例成为app包的成员。
 app.config.from_object(Config)
 
-from app import routes  # 从app包中导入模块routes
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models  # 从app包中导入模块routes
