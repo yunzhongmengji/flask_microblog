18fb1fdd509af8ca68b3c0031861ca2992ba6147
yunzhongmengji <1806789313@qq.com>
2022-05-02 14:29:06 +0800

    flask form

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,7 @@
 from flask import Flask  # 从flask包中导入Flask类
-
+from config import Config
 
 app = Flask(__name__)  # 将Flask类的实例 赋值给名为 app 的变量。这个实例成为app包的成员。
-
-print('等会谁（哪个包或模块）在使用我：', __name__)
+app.config.from_object(Config)
 
 from app import routes  # 从app包中导入模块routes
