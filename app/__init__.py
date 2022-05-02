2a5c9541b11df1496ddd554255b26509204df6c2
yunzhongmengji <1806789313@qq.com>
2022-05-02 21:46:22 +0800

    login_register

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,13 @@ from config import Config  # 从config模块导入Config类
 from flask_sqlalchemy import SQLAlchemy  # 丛包中导入类
 from flask_migrate import Migrate
 
+from flask_login import LoginManager
+
 app = Flask(__name__)  # 将Flask类的实例 赋值给名为 app 的变量。这个实例成为app包的成员。
 app.config.from_object(Config)
 
+login = LoginManager(app)
+login.login_view = 'login'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,18 @@
-from flask import render_template, flash, redirect, url_for
-from app import app  # 从app包中导入 app这个实例
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
+
+from app import app, db  # 从app包中导入 app这个实例
 
 # 2个路由
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
+from app.models import User
 
 
 @app.route('/')
 @app.route('/index')
-# 1个视图函数
+@login_required
 def index():
-    user = {'username': 'Miguel'}
     posts = [  # 创建一个列表：帖子。里面元素是两个字典，每个字典里元素还是字典，分别作者、帖子内容。
         {
             'author': {'username': 'John'},
@@ -20,14 +23,43 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()  # 表单实例化对象
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me='
-              '{}'.format(form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')  # 重定向到next界面
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title="Register", form=form)
